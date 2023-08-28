d89361661e2d9ec36d23c85fe3bd3dd61e77fce3
epode4 <epode4@gamil.com>
2023-08-28 15:06:18 +0900

    login 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
@@ -18,3 +19,20 @@ def signup(request):
     }
 
     return render(request, 'signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:login')
+    else:
+        form = CustomAuthenticationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'login.html', context)
